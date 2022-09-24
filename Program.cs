df5e3a0e9318922f6a35fc3252339c8f32627d1f
VickeS <vic1997@users.noreply.github.com>
2022-09-24 18:05:41 +0200

    Adding Darkmode and toggle setting on UI page.

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddMudServices();
+builder.Services.AddSingleton<GlobalValuesManager>();
 
 var app = builder.Build();
 
